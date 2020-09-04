54d0fefb9c99672539f64caa6adbc2e2a776da8a
ithieunguyen2911 <usahieunguyen2018@gmail.com>
2020-09-04 11:17:35 +0700

    push code Bootstrap & Responsive

---
+++ resources/js/scripts.js
@@ -0,0 +1,10 @@
+// $(document).ready(
+//     function(){
+//         $('a').click(function(event){
+//             $('html, body').animate({
+//                 scrollTop: $( $.attr(this, 'href') ).offset().top
+//             }, 1000);
+//             event.preventDefault();
+//         });
+//     }
+// )
